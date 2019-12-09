829f918a0d4d94da5fd3004768421974fbe056e7
Weikoi <huangzengrui@yahoo.com>
2019-12-09 16:44:57 +0800

    modify

--- leetcode/medium/6_Z字变换.py
+++ leetcode/medium/6_Z字变换.py
@@ -19,3 +19,31 @@ class Solution:
             curr_row += go_down
 
         return "".join(result)
+
+    def convert2(self, s, numRows):
+        total_list = [[] for i in range(min(numRows, len(s)))]
+
+        if numRows < 2:
+            return s
+
+        down = 1
+        row = 0
+
+        for i in s:
+            total_list[row].append(i)
+            if (down == 1 and row == numRows - 1) or (down == -1 and row == 0):
+                down = -1 * down
+            if down == 1:
+                row += 1
+            elif down == -1:
+                row -= 1
+
+        return_str = ""
+        for each in total_list:
+            return_str += "".join(each)
+
+        return return_str
+
+
+if __name__ == '__main__':
+    print(Solution().convert2("LEETCODEISHIRING", 3))
